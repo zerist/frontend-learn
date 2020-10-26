99d517f2e8cdda1ef4ce63901a53d68678b8e0d4
zerist <15871792739@163.com>
2020-10-26 16:36:45 +0800

    tree travel

---
+++ algorithm/tree/travel.js
@@ -0,0 +1,50 @@
+const root = {
+    val: "A",
+    left: {
+        val: "B",
+        left: {
+            val: "D"
+        },
+        right: {
+            val: "E"
+        }
+    },
+    right: {
+        val: "C",
+        right: {
+            val: "F"
+        }
+    }
+};
+
+function preorder(root) {
+    if (!root) {
+        return
+    }
+
+    console.log('current node: ', root.val)
+    preorder(root.left)
+    preorder(root.right)
+}
+
+function midorder(root) {
+    if (!root) {
+        return
+    }
+
+    midorder(root.left)
+    console.log('current node: ', root.val)
+    midorder(root.right)
+}
+
+function posorder(root) {
+    if (!root) {
+        return
+    }
+
+    posorder(root.left)
+    posorder(root.right)
+    console.log(root.val)
+}
+
+posorder(root)
